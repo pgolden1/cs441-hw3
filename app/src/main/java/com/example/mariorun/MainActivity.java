680d86290edab154749a5b36cbb8e75faec70871
Peter Golden <petergolden@Sharon-Galaxy-S6.localdomain>
2019-02-17 23:45:55 -0500

    Designed main and game activities

--- app/src/main/java/com/example/mariorun/MainActivity.java
+++ app/src/main/java/com/example/mariorun/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mariorun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView skyimg = (ImageView) findViewById(R.id.sky);
+        int skyResource = getResources().getIdentifier("@drawable/sky", null, this.getPackageName());
+        skyimg.setImageResource(skyResource);
     }
 }

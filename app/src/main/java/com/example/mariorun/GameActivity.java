9756489abc0d05fcb2819158fc636a546a0372fe
Peter Golden <petergolden@Sharon-Galaxy-S6.localdomain>
2019-02-24 22:37:32 -0500

    added marioview class for game logic

--- app/src/main/java/com/example/mariorun/GameActivity.java
+++ app/src/main/java/com/example/mariorun/GameActivity.java
@@ -1,13 +1,120 @@
 package com.example.mariorun;
 
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.view.View;
+import android.content.Context;
+
+
+import java.io.IOException;
 
 public class GameActivity extends AppCompatActivity {
+
+    public class MarioView extends SurfaceView implements Runnable{
+
+        MovementState movement;
+        ImageView mario;
+        //ImageButton jumpButton, walkLeftButton, walkRightButton, menuButton;
+        SurfaceHolder ourHolder;
+        Canvas canvas;
+        Paint paint;
+        Thread gameThread = null;
+
+        public MarioView(Context c, String pkgnm) {
+            super(c);
+            movement = new MovementState(getResources(), pkgnm);
+            ourHolder = getHolder();
+            paint = new Paint();
+
+
+        }
+
+        @Override
+        public void run(){
+            while(true) draw();
+        }
+
+        public void draw(){
+
+            if (ourHolder.getSurface().isValid()) {
+                // Lock the canvas ready to draw
+                canvas = ourHolder.lockCanvas();
+
+
+                // Draw the background color
+                canvas.drawColor(Color.argb(255, 160, 177, 250));
+
+                /* Choose the brush color for drawing
+
+                paint.setColor(Color.argb(255, 255, 255, 255));
+                canvas.drawLine(0, 0, 300, y, paint);
+
+
+                canvas.drawCircle(posx, posy, 30l, paint);*/
+
+                ourHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+
+        public void onResume(){
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+
+        public void pause(){
+            try {
+                gameThread.join();
+            } catch (InterruptedException e) {
+                Log.e("Error:", "joining thread");
+            }
+        }
+    }
+
+
+    MarioView marioview;
+    ImageButton menuButton;
+    ImageView sky;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        //setContentView(R.layout.activity_game);
+
+
+        marioview = new MarioView(this, this.getPackageName());
+        setContentView(marioview);
+
     }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        marioview.onResume();
+    }
+            //mario.setImageResource(movement.getCurrentState());
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Tell the gameView pause method to execute
+        marioview.pause();
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/mariorun/MainActivity.java
+++ app/src/main/java/com/example/mariorun/MainActivity.java
@@ -1,21 +1,70 @@
 package com.example.mariorun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.view.View;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView mario;
+    ImageButton playButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView mario = (ImageView) findViewById(R.id.mario);
+        mario = (ImageView) findViewById(R.id.mario);
+        playButton =(ImageButton)findViewById(R.id.play);
 
-        MovementState movement = new MovementState(getResources(), this.getPackageName());
 
-        mario.setImageResource(movement.getCurrentState());
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(i);
+            }
+        });
 
     }
+
+    public class MarioView extends SurfaceView implements Runnable{
+
+        ImageButton jump, walkLeft, walkRight;
+        ImageView mario;
+
+        MovementState movement;
+
+        public MarioView(Context c, MovementState m, ImageView mario, ImageButton jumpButton, ImageButton walkLeftButton, ImageButton walkRightButton){
+            super(c);
+            this.mario = mario;
+            this.movement = movement;
+            jump = jumpButton;
+            walkLeft = walkLeftButton;
+            walkRight = walkRightButton;
+        }
+
+        @Override
+        public void run(){
+            //mario.setImageResource(movement.getCurrentState());
+        }
+
+
+
+
+
+    }
+
+
 }
